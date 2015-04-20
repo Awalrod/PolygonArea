7273576141856e7a0c18be43a01b878839fc45de
Alex Walrod <awalrod123@gmail.com>
2015-04-19 20:36:01 -0500

    comments

--- polygon.py
+++ polygon.py
@@ -10,6 +10,7 @@ class Point:
 	def asTuple(self):
 		return (self.x , self.y)
 		
+		
 class Line:
 	endPoints = []
 	
@@ -43,7 +44,7 @@ class Triangle:
 		for length in self.sideLengths:
 			p += length
 		p = p/2
-		a = math.sqrt(p*(p-self.sideLengths[0])*(p-self.sideLengths[1])*(p-self.sideLengths[2]))
+		a = math.sqrt(p*(p-self.sideLengths[0])*(p-self.sideLengths[1])*(p-self.sideLengths[2]))#heron's formula
 		return a
 		
 class Polygon:
@@ -54,16 +55,18 @@ class Polygon:
 		self.points = points
 		
 	def drawTriangles(self):
+		#adds a bunch of triangles to self.triangles
+		#I suspect this is where the but is but can't seem to figure it out
 		numOfPoints = len(self.points)
 		x = 1
 		y = 2
 		while y < numOfPoints:
-			source = self.points[0]
-			p1 = self.points[x]
-			p2 = self.points[y]
+			p1 = self.points[0]
+			p2 = self.points[x]
+			p3 = self.points[y]
 			x += 1
 			y += 1
-			newTriangle = Triangle([source,p1,p2])
+			newTriangle = Triangle([p1,p2,p3])
 			self.triangles.append(newTriangle)
 			
 			
@@ -71,7 +74,7 @@ class Polygon:
 		self.drawTriangles()
 		area = 0
 		for triangle in self.triangles:
-			print triangle.area()
+			print triangle.area()#shows the bug, every triangle is the same				
 			area += triangle.area()
 		return area
 		
@@ -81,8 +84,7 @@ def main():
 	points = []
 	for x in range(sideNum):
 		x,y = map(float,inp.readline().split())
-		
-		points.append(Point(x,y))
+		points.append(Point(x,y))#making a list of points, already in ccw order
 	poly = Polygon(points)
 	print(poly.area())
 	
